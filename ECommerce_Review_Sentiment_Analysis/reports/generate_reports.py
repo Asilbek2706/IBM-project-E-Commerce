789ef30f58e1993772983e816a1baa6a0f65a7f7
Asilbek2706 <asilbekkaromatov2@gmail.com>
2025-12-17 17:55:49 +0500

    reports

---
+++ ECommerce_Review_Sentiment_Analysis/reports/generate_reports.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+df = pd.read_csv("../data/reviews_clean.csv")
+X = df['clean_review']
+y = df['sentiment']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Vectorization
+vectorizer = CountVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
+y_pred = model.predict(X_test_vec)
+
+report = classification_report(y_test, y_pred)
+with open("../reports/classification_report.txt", "w") as f:
+    f.write(report)
+
+cm = confusion_matrix(y_test, y_pred)
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=model.classes_, yticklabels=model.classes_)
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.tight_layout()
+plt.savefig("../reports/confusion_matrix.png")
+plt.close()
+
+print("Reports saved: classification_report.txt va confusion_matrix.png")
